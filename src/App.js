e3c7195c731d30c2184ea107c025d7305ba7855e
MacreenSmith <macreensmith@gmail.com>
2023-07-20 10:56:51 -0400

    Added counters with an addition, subtraction, and reset button.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import Objects from './Objects'
+import { useState } from 'react';
+
 
 function App() {
+
+
+  const [counter, setCounter] = useState(0)
+
+  const handleIncrement = () => {
+    setCounter(counter + 1)
+  }
+  const handleDecrement = () => {
+    setCounter(counter - 1)
+  }
+
   return (
     <div className="App">
-     <h1>React Drills</h1>
-
+      <h1>React Drills</h1>
+      <h1>{counter}</h1>
+      <button onClick={handleIncrement}> + </button>
+      <button onClick={handleDecrement}> - </button>
+      <button onClick={() => {setCounter(0)}}>Reset</button>
     </div>
   );
 }
---
+++ src/Objects.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Objects() {
+    const zuko ={
+        firstName: 'Zuko',
+        age: 15,
+        email: 'something@hotmail.com'
+    }
+    //const{firstName, age, email} = zuko
+    
+    return Object.keys(zuko).map((key, index)=>{
+    return (
+       <h2>
+        {key}: {zuko[key]}
+       </h2>
+    )
+    })
+}
+export default Objects
